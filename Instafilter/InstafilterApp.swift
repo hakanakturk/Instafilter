d9ebdd61c7b8285d0256f8a87d620b60b747aa4f
Hakan Aktürk <hakanakturk@outlook.com>
2023-05-11 12:26:27 +0300

    initial commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Hakan Aktürk on 9.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
